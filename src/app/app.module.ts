e5c89981bdc0b54ee22b1a43061d81b93c469ea1
Rayli Raykov <rayli.raykov@gmail.com>
2023-01-09 21:40:13 -0500

    set up drag and drop thing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,6 +51,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -141,6 +142,7 @@ import { WaitlistComponent } from './waitlist/waitlist.component';
         MatToolbarModule,
         MatTooltipModule,
         MatTreeModule,
+        DragDropModule,
         FormsModule,
         ReactiveFormsModule,
         HttpClientModule,
--- src/app/tables-dashboard/tables-dashboard.component.ts
+++ src/app/tables-dashboard/tables-dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Message } from '../_models/message.model';
-import { MessagesService } from './../_services/messages.service';
+
 
 @Component({
   selector: 'app-tables-dashboard',
@@ -8,28 +7,10 @@ import { MessagesService } from './../_services/messages.service';
   styleUrls: ['./tables-dashboard.component.scss']
 })
 export class TablesDashboardComponent implements OnInit {
-  message: Message = {
-    channelId: 'a31f78766da04f9e95ce52a85cf13bdd',
-    to: '14123549611',
-    type: 'text',
-    content: {
-      text: 'Your table is ready, please come take a seat!'
-    }
-  };
 
-  constructor(
-    public messagesService: MessagesService,
-  ) { 
-
-  }
+  constructor() {}
 
   ngOnInit(): void {
-  }
-
-  test(){
-    console.log("sending message");
-    this.messagesService.createMessage(this.message);
-    console.log("Hopefully it sent");
   }
 
 }
