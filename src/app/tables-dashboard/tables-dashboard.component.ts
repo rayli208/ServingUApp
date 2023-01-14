8d36d00bda57590ada63735687e3830139dbc6c4
Rayli Raykov <rayli.raykov@gmail.com>
2023-01-14 15:58:55 -0500

    log out all the x and y coordinates of table

--- src/app/tables-dashboard/tables-dashboard.component.ts
+++ src/app/tables-dashboard/tables-dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkDragEnd } from '@angular/cdk/drag-drop';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 
 @Component({
@@ -6,11 +7,49 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './tables-dashboard.component.html',
   styleUrls: ['./tables-dashboard.component.scss']
 })
-export class TablesDashboardComponent implements OnInit {
+export class TablesDashboardComponent {
+  @ViewChild('tableView', { read: ElementRef }) tableView: ElementRef;
 
-  constructor() {}
+  tables = [
+    { number: 1,
+      shape: "square",
+      color: "#000000",
+      bgColor: "#ffffff",
+      seats: 6,
+    }, 
+    { number: 2,
+      shape: "circle",
+      color: "#000000",
+      bgColor: "#ffffff",
+      seats: 6,
+    }, 
+    { number: 3,
+      shape: "circle",
+      color: "#000000",
+      bgColor: "#ffffff",
+      seats: 6,
+    }, 
+    { number: 4,
+      shape: "hexgon",
+      color: "#000000",
+      bgColor: "#ffffff",
+      seats: 6,
+    }, 
+    { number: 5,
+      shape: "hexgon",
+      color: "#000000",
+      bgColor: "#ffffff",
+      seats: 6,
+    },   ];
 
-  ngOnInit(): void {
+  dragEnd(event: CdkDragEnd, table: any) {
+    const droppedTable = event.source.element.nativeElement;
+    const tableViewRect = this.tableView.nativeElement.getBoundingClientRect();
+    const droppedTableRect = droppedTable.getBoundingClientRect();
+
+    console.log(`Table ${table.number} Position:`, {
+      x: droppedTableRect.left - tableViewRect.left,
+      y: droppedTableRect.top - tableViewRect.top
+    });
   }
-
 }
