ba935ccb729546aa994b1c90ebe2c6d9c52db5fc
Rayli Raykov <rayli.raykov@gmail.com>
2023-06-05 11:28:20 -0400

    update to angular 16

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { AngularFireAuth } from '@angular/fire/compat/auth';
@@ -7,7 +7,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Injectable({
     providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
     constructor(private router: Router, private afAuth: AngularFireAuth) {
 
