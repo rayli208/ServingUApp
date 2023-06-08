a8cdb26db8d9c3480bf0debd3c2e550eb223c10e
Rayli Raykov <rayli.raykov@gmail.com>
2023-06-07 20:30:22 -0400

    Clean up all signin and signup forms

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../_services/auth.service'
-import { UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { PhoneNumberFormatDirective } from '../_directives/phone-number.directive';
 
 @Component({
@@ -9,50 +9,70 @@ import { PhoneNumberFormatDirective } from '../_directives/phone-number.directiv
     templateUrl: './signup.component.html',
     styleUrls: ['./signup.component.scss'],
 })
-
 export class SignupComponent implements OnInit {
 
     isProgressVisible: boolean;
-    signupForm: UntypedFormGroup;
+    signupForm: FormGroup;
     firebaseErrorMessage: string;
 
-    constructor(private authService: AuthService, private router: Router) {
+    constructor(private authService: AuthService, private router: Router, private _formBuilder: FormBuilder) {
         this.isProgressVisible = false;
         this.firebaseErrorMessage = '';
     }
 
     ngOnInit(): void {
-        if (this.authService.userLoggedIn) {                       // if the user's logged in, navigate them to the dashboard (NOTE: don't use afAuth.currentUser -- it's never null)
+        if (this.authService.userLoggedIn) {
             this.router.navigate(['/profile-dashboard']);
         }
 
-        this.signupForm = new UntypedFormGroup({
-            'owner': new UntypedFormControl('', Validators.required),
-            'location_name': new UntypedFormControl('', Validators.required),
-            'description': new UntypedFormControl('', Validators.required),
-            'email': new UntypedFormControl('', [Validators.required, Validators.email]),
-            'phone': new UntypedFormControl('', Validators.required),
-            'website': new UntypedFormControl('', Validators.required),
-            'address': new UntypedFormControl('', Validators.required),
-            'zip': new UntypedFormControl('', Validators.required),
-            'school': new UntypedFormControl('', Validators.required),
-            'pin': new UntypedFormControl('', Validators.required),
-            'password': new UntypedFormControl('', Validators.required),
+        this.signupForm = this._formBuilder.group({
+            stepOne: this._formBuilder.group({
+                owner: ['', Validators.required],
+                location_name: ['', Validators.required],
+                description: ['', Validators.required]
+            }),
+            stepTwo: this._formBuilder.group({
+                email: ['', [Validators.required, Validators.email]],
+                phone: ['', Validators.required],
+                website: ['', Validators.required]
+            }),
+            stepThree: this._formBuilder.group({
+                address: ['', Validators.required],
+                zip: ['', Validators.required],
+                school: ['', Validators.required]
+            }),
+            stepFour: this._formBuilder.group({
+                pin: ['', Validators.required],
+                password: ['', Validators.required],
+                confirmPassword: ['', Validators.required]
+            }, { validator: this.checkPasswords })
         });
     }
 
+    checkPasswords(group: FormGroup) { // here we have the 'passwords' group
+        let pass = group.get('password').value;
+        let confirmPass = group.get('confirmPassword').value;
+
+        return pass === confirmPass ? null : { notSame: true }
+    }
+
     signup() {
-        if (this.signupForm.invalid)                            // if there's an error in the form, don't submit it
+        if (this.signupForm.invalid)
             return;
 
         this.isProgressVisible = true;
-        this.authService.signupUser(this.signupForm.value).then((result) => {
-            if (result == null)                                 // null is success, false means there was an error
+        this.authService.signupUser({
+            ...this.signupForm.controls.stepOne.value,
+            ...this.signupForm.controls.stepTwo.value,
+            ...this.signupForm.controls.stepThree.value,
+            ...this.signupForm.controls.stepFour.value
+        }).then((result) => {
+            if (result == null)
                 this.router.navigate(['/profile-dashboard']);
             else if (result.isValid == false)
                 this.firebaseErrorMessage = result.message;
 
-            this.isProgressVisible = false;                     // no matter what, when the auth service returns, we hide the progress indicator
+            this.isProgressVisible = false;
         }).catch(() => {
             this.isProgressVisible = false;
         });
