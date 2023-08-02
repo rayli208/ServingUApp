875fbb162b1ba0bb9b1cc1ae7a0717f609976fd6
Rayli Raykov <rayli.raykov@gmail.com>
2023-08-01 20:06:04 -0400

    random stage

--- src/app/_models/contact.model.ts
+++
@@ -1,9 +0,0 @@
-export class Contact {
-    name: string;
-    phoneNumber: string;
-    waitTime: number;
-    totalParty: number;
-    reservationMade: string;
-    estimatedTime: string;
-    hasRecievedText: boolean;
-}
--- src/app/_models/job.model.ts
+++
@@ -1,9 +0,0 @@
-export class Job {
-    id: string;
-    uid: string;
-    title: string;
-    icon: string;
-    description: string;
-    totalPositions: number;
-    employmentType: string;
-}
